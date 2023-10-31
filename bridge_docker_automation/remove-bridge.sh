388debfbf5ce71cb0af0632fc400008d0da7da5c
jfluckiger <jfluckiger@tableau.com>
2023-10-31 16:16:57 -0500

    remove-agent.zsh

--- bridge_docker_automation/remove-bridge.sh
+++ bridge_docker_automation/remove-bridge.sh
@@ -17,8 +17,10 @@ fi
 
 for i in $(seq 0 $(($NUM_AGENTS - 1))); do
     AGENT_PREFIX=$(yq '.bridge.agent_name' bridge_config.yml)
-    BRIDGE_ENV_FILE=$AGENT_PREFIX-$i.env
-    echo "Removing container $AGENT_PREFIX-$i and env file"
+    AGENT_NAME="${AGENT_PREFIX}-$i"
+    BRIDGE_ENV_FILE=$AGENT_NAME.env
+    echo "Removing agent $AGENT_NAME"
     rm -f ${BRIDGE_ENV_FILE}
-    docker rm -f $AGENT_PREFIX-$i
+    docker rm -f $AGENT_NAME
+    /usr/local/bin/python3.11 ~/_git/dcli/start.py --remove_agent --agent_name $AGENT_NAME
 done
