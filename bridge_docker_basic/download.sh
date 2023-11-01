7c809ba3bb0dfc1c77b3cccc27700b0673a7fc55
jfluckiger <jfluckiger@tableau.com>
2023-11-01 12:30:41 -0500

    remove-agent.zsh

--- bridge_docker_basic/download.sh
+++ bridge_docker_basic/download.sh
@@ -1,6 +1,8 @@
 
 #Download Tableau Bridge rpm
+
 curl -OL https://downloads.tableau.com/tssoftware/TableauBridge-20233.23.1017.0948.x86_64.rpm
+
 #Download database drivers for mysql and postgres
 curl -OL https://dev.mysql.com/get/mysql80-community-release-el7-7.noarch.rpm
 curl -OL https://jdbc.postgresql.org/download/postgresql-42.3.4.jar
