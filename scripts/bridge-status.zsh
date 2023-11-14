da77a554cfd91652e023dd13f145fe1ebcacbee3
jfluckiger <jfluckiger@tableau.com>
2023-11-14 03:38:13 -0600

    README.md

--- scripts/bridge-status.zsh
+++
@@ -1,5 +0,0 @@
-
-# script to display bridge agent status by calling tableau cloud APIs
-
-source ~/.zshrc
-dcli --bridge_status
--- scripts/jobs-report.zsh
+++
@@ -1,5 +0,0 @@
-
-# script to call tableau cloud APIS and display a recent jobs report.
-
-source ~/.zshrc
-dcli --jobs
